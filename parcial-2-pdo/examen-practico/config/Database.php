9737e27aff0855a6e8a26a203a9c078e965ee489
Michelle Quintero <qbmichelle175@gmail.com>
2026-04-26 22:12:05 -0700

    Examen practico - Archivo Database

---
+++ parcial-2-pdo/examen-practico/config/Database.php
@@ -0,0 +1,25 @@
+<?php
+// MICHELLE QUINTERO BONILLA
+class Database{
+    private $host = "localhost";
+    private $dbname = "productos";
+    private $username = "root";
+    private $password = "";
+    private $connection;
+
+    public function __construct(){
+        try{
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
+            $this->connection = new PDO($dsn,$this->username,$this->password);
+
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); #Array asociativo
+        } catch(PDOException $e){
+            die("Error de conexión:" .$e->getMessage());
+        }        
+    }
+    public function getConnection(){
+    return $this->connection;
+    }
+}
+?>
