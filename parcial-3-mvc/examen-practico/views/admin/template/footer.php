d6a424875cee417568f8820ffe4b24df560f75df
Michelle Quintero <qbmichelle175@gmail.com>
2026-05-05 20:30:12 -0700

    Examen practico - Archivo footer

---
+++ parcial-3-mvc/examen-practico/views/admin/template/footer.php
@@ -0,0 +1,46 @@
+<!--MICHELLE QUINTERO BONILLA-->
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+  <footer class="text-center text-lg-start bg-light text-muted">
+    <section class="p-1">
+      <div class="container text-center text-md-start mt-5">
+        <div class="row nt-3">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auo mb-4">
+            <h6>LOGO AQUI</h6>
+          </div>
+
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb4">DESARROLLO WEB II</h6>
+            <p>
+              Aplicacion Web con PHP, MySQL, PDO Y BootStrap.
+            </p>
+          </div>
+
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auo mb-4">
+            <h6 class="text-uppercase fw-bold mb4"> ENLACES DE INTERES</h6>
+            <p>
+              XXXXXX
+            </p>
+          </div>
+
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auo mb-4">
+            <h6 class="text-uppercase fw-bold mb4">CONTACTO</h6>
+              <p>
+                <i class="fa-solid fa-phone me-2"></i> 669 266 3632 <br>
+                <i class="fa-solid fa-envelope me-2"></i> qbmichelle175@gmail.com <br>
+                <i class="fa-solid fa-school me-2"></i> Facultad de Informática Mazatlán, UAS
+              </p>
+          </div>
+        </div>
+        <div class="row nt-3">
+
+        </div>
+
+      </div>
+    </section>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.25);">
+      - Clase Desarrollo Web II. MVC. PHP. PDO. MySQL. BOOTSRAP.
+    </div>
+  </footer>
+</html>
