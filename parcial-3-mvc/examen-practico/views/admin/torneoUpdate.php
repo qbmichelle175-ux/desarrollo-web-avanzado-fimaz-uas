726104fd150e7d57e2a5569ce1b6838895b32679
Michelle Quintero <qbmichelle175@gmail.com>
2026-05-05 20:31:34 -0700

    Examen practico - Archivo torneoUpdate

---
+++ parcial-3-mvc/examen-practico/views/admin/torneoUpdate.php
@@ -0,0 +1,22 @@
+<!--MICHELLE QUINTERO BONILLA-->
+<?php
+    require_once ("../../controllers/torneosController.php");
+
+    $objController = new torneosController();
+
+    $id = $_POST['txtId'];   
+    $nombreTorneo = $_POST['txtNombreTorneo'];
+    $organizador = $_POST['txtOrganizador'];
+    $patrocinadores = $_POST['txtPatrocinadores'];
+    $sede = $_POST['txtSede'];
+    $categoria = $_POST['txtCategoria'];
+    $premio1 = $_POST['txtPremio1'];
+    $premio2 = $_POST['txtPremio2'];
+    $premio3 = $_POST['txtPremio3'];
+    $otroPremio = $_POST['txtOtroPremio'];
+
+    $objController->updateTorneo ($id, $nombreTorneo, $organizador, $patrocinadores, $sede, $categoria,
+    $premio1, $premio2, $premio3, $otroPremio);
+
+
+?>
