f0dade7ee7ce2ddd3c5c1354b17bb4b28f220a6b
Michelle Quintero <qbmichelle175@gmail.com>
2026-05-05 20:28:20 -0700

    Examen practico - Archivo DataBase

---
+++ parcial-3-mvc/examen-practico/config/DataBase.php
@@ -0,0 +1,23 @@
+<?php
+// MICHELLE QUINTERO BONILLA
+class DataBase{
+    private $host = "localhost";
+    private $db = "proyecto";
+    private $user = "root";
+    private $password = "";
+
+    public function __construct(){
+        //Constructor
+    }
+
+    public function connect(){
+        try{
+            $PDO = new PDO ("mysql:host=".$this->host.";dbname=".$this->db,$this->user,
+            $this->password);
+            return $PDO;
+        } catch(PDOException $e){
+            return $e->getMessage();
+        }    
+    }
+}
+?>
